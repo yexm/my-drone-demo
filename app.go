379bc167df50c264152b13d7682f8f22b762875c
yexm <yexingmin06221@126.com>
2019-12-25 09:29:59 +0800

    test

--- app.go
+++ app.go
@@ -8,7 +8,7 @@ import (
 func main() {
     m := linkedhashmap.New() // empty (keys are of type int)
     m.Put("aaa", "b")            // 2->b
-    m.Put("ccc", "x")            // 2->b, 1->x (insertion-order)
+    m.Put("cdc", "x")            // 2->b, 1->x (insertion-order)
     m.Put("cacs", "a")            // 2->b, 1->a (insertion-order)
     b, _ := m.ToJSON()
     fmt.Println(string(b))
